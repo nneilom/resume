2ee26f2b5eb71c1d30e769549307c89a366870ff
Neilom <nil.porvani@gmail.com>
2023-02-16 19:55:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector(".sidebar").addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
